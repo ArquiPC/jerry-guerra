405a5886b3893683c87c156bf849736e86958232
ArquiPC <jayber1990@gmail.com>
2026-02-24 17:23:17 +0000

    Conectando galeria dinamica con Supabase

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ const supabaseUrl = 'https://lrjsideqdmiekflpught.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyanNpZGVxZG1pZWtmbHB1Z2h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NjE5NTUsImV4cCI6MjA4NzQzNzk1NX0.fVk9OMKOpV25DMNra-Q5-6iRk3u3ZH6Ye2G-EuBlu28';
 
 // Usamos el objeto global de la librería cargada en el HTML
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 // 2. FUNCIÓN DE WHATSAPP (Disponible globalmente para el onclick)
 function enviarWhatsApp() {
